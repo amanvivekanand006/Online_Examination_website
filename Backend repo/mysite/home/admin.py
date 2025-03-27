85e0e60e6e3f6b92864581104ec8115cab9389af
v aman <amanamn2000@gmail.com>
2025-03-28 02:15:43 +0530

    backend removed

--- Backend repo/mysite/home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . models import *
-# from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
-
-admin.site.register(Profile)
-admin.site.register(TestQuestions)
-admin.site.register(Test)
-admin.site.register(getStudentAnswer)
-admin.site.register(takeanswers)
