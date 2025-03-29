93b14626fa6e383f7c7a47c540aa29c68eca0046
v aman <amanamn2000@gmail.com>
2025-03-29 14:27:33 +0530

    adfad

--- Frontend_repo/online/src/Components/Landing.jsx
+++ Frontend_repo/online/src/Components/Landing.jsx
@@ -19,16 +19,6 @@ const Footer = () => {
 };
 
 const LandingPage = () => {
-  const [message, setMessage] = useState("");
-  setMessage(localStorage.data.message)
-  toast.success(message, {
-    position: "top-right",
-    autoClose: 3000, // Close after 3 seconds
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-});
   return (
     <div>
       <Landingnav/>
